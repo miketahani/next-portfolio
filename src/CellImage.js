93789d0b7ee20399db2086633094120c5d1970c0
Mike Tahani <me@miketahani.com>
2021-03-22 21:37:38 -0700

    extract CellImage component

---
+++ src/CellImage.js
@@ -0,0 +1,20 @@
+import { getBBox, fitImage } from './util'
+
+export default function CellImage ({ image, polygon, position, index }) {
+  if (!polygon) return null;
+
+  const bbox = getBBox(polygon)
+  const { width, height } = fitImage(image, bbox)
+
+  return (
+    <image
+      href={image.filePath}
+      width={~~width}
+      height={~~height}
+      clipPath={`url(#poly-${index})`}
+      x={position[0] - width / 2}
+      y={position[1] - height / 2}
+      key={image.id}
+    />
+  )
+}
