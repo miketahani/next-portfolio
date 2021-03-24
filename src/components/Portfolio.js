ac21166ee0d76074325355fd93c888c1c0575bcd
Mike Tahani <me@miketahani.com>
2021-03-24 14:32:08 -0700

    pass imageManifest to vis component; add modal logic structure

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -5,18 +5,33 @@ import { useObserver } from '../hooks/useObserver'
 
 import Visualization from './Visualization'
 
+import imageManifest, { postsById } from '../util/loadManifest'
+
 export default function Portfolio () {
   const [page, setPage] = useState(0)
+  const [modal, setModal] = useState(false)
+
   const observerTargetNode = useRef()
 
   const nextPage = useCallback(() => setPage(prevPage => prevPage + 1), [])
   useObserver(observerTargetNode, nextPage)
 
+  // If there's existing modal content, close the modal; else set the content data
+  //   to the passed-in item index (which should correspond to the fileId)
+  const toggleModal = portfolioItemIndex =>
+    setModal(modal !== false ? false : imageManifest[portfolioItemIndex])
+
   return (
     <>
-      <Visualization page={page} />
+      <Visualization
+        page={page}
+        imageManifest={imageManifest}
+        onSelectPortfolioItem={toggleModal}
+      />
 
       <div id="ObserverTarget" ref={observerTargetNode} />
+
+      {!!modal && null}
     </>
   )
 }
