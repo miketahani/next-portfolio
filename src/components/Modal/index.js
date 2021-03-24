75595373809a994e3d1d03b0e1cd1aa14a439f3e
Mike Tahani <me@miketahani.com>
2021-03-24 14:03:31 -0700

    add Modal component

---
+++ src/components/Modal/index.js
@@ -0,0 +1,29 @@
+import React, { useEffect, useRef } from 'react'
+import ReactDOM from 'react-dom'
+
+import './styles.css'
+
+const modalRoot = document.getElementById('modal-root')
+
+function createModalElement () {
+  const el = document.createElement('div')
+  el.classList.add('Modal')
+  return el
+}
+
+export function Modal ({ children }) {
+  const el = useRef(createModalElement())
+
+  useEffect(() => {
+    const node = el.current
+    modalRoot.appendChild(node)
+    return () => modalRoot.removeChild(node)
+  }, [])
+
+  if (!el.current) return null
+
+  return ReactDOM.createPortal(
+    children,
+    el.current
+  )
+}
