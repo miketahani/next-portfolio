a39bfb18224c6559800b16693ff410afaf7cca4c
Mike Tahani <me@miketahani.com>
2021-03-24 18:24:22 -0700

    add more data to modal; styling

--- src/components/ImageDetailModal/index.js
+++ src/components/ImageDetailModal/index.js
@@ -1,15 +1,26 @@
+import { timeFormat } from 'd3-time-format'
+
 import Modal from '../Modal'
 
 import {
   ImageDetailModalContainer,
+  Header,
+  Title,
   Hero,
   HeroCaption,
   PostCaption,
   PostImages,
-  PostImage
+  PostImageContainer,
+  PostImage,
+  Footer,
+  PostDate,
+  NoteCount,
+  Tag
 } from './styles'
 
 import { IMAGES_DIRECTORY } from '../../config'
+
+const formatTime = timestamp => timeFormat('%d %b %Y')(timestamp * 1000);
 
 // Creates an object that is passed to `dangerouslySetInnerHTML`. There's no
 // user input involved here, so this is safe.
@@ -19,29 +30,45 @@ export default function ImageDetailModal ({ post, image, onClose }) {
   return (
     <Modal>
       <ImageDetailModalContainer>
-        <div onClick={onClose}>close</div>
+        <Header>
+          <PostDate>{formatTime(post.timestamp)}</PostDate>
+          <NoteCount>❤︎ {post.note_count}</NoteCount>
+          <div onClick={onClose}>close</div>
+        </Header>
 
-        <Hero src={`${IMAGES_DIRECTORY}/${image.filename}`} alt={image.caption} />
+        <Hero src={`${IMAGES_DIRECTORY}/${image.filename}`} alt={image.caption || post.summary} />
 
         <HeroCaption>{image.caption}</HeroCaption>
 
         {post.photos.length > 1 &&
           <PostImages>
             {post.photos.map(photo =>
-              <PostImage
-                src={`${IMAGES_DIRECTORY}/${photo.meta.filename}`}
-                isCurrentlySelected={photo.meta.fileId === image.fileId}
-                alt={photo.meta.caption}
-                key={photo.meta.postIndex}
-              />
+              // Container is display: flex, so surround the <img> with a
+              // display: block element so image scales correctly
+              <PostImageContainer key={photo.meta.photoIndex}>
+                <PostImage
+                  src={`${IMAGES_DIRECTORY}/${photo.meta.filename}`}
+                  isCurrentlySelected={photo.meta.fileId === image.fileId}
+                  alt={photo.meta.caption || `(image ${photo.meta.photoIndex + 1}/${post.photos.length})`}
+                />
+              </PostImageContainer>
             )}
           </PostImages>
         }
 
         <PostCaption dangerouslySetInnerHTML={createCaption(post.caption)} />
 
+        <Footer>
+          {post.tags.length > 0 &&
+            <>
+              <span>Tags</span>
+              {post.tags.map(tag =>
+                <Tag key={tag}>{tag}</Tag>
+              )}
+            </>
+          }
+        </Footer>
       </ImageDetailModalContainer>
     </Modal>
   )
 }
-
--- src/components/ImageDetailModal/styles.js
+++ src/components/ImageDetailModal/styles.js
@@ -15,7 +15,18 @@ export const ImageDetailModalContainer = styled.div`
   animation: ${bouncein} .2s cubic-bezier(0.175, 0.885, 0.32, 1.275);
 `
 
+export const Header = styled.header`
+  display: flex;
+  font-weight: bold;
+  font-size: 1.2rem;
+  text-transform: uppercase;
+`
+
+export const Title = styled.div`
+`
+
 export const Hero = styled.img`
+  max-width: 100%;
   max-height: 50%;
 `
 
@@ -30,10 +41,37 @@ export const PostCaption = styled.div`
 export const PostImages = styled.div`
   width: 90%;
   display: flex;
+  align-items: center;
+  flex-direction: row;
   flex-wrap: wrap;
 `
 
-export const PostImage = styled.img`
+export const PostImageContainer = styled.div`
   width: 25%;
 `
 
+export const PostImage = styled.img`
+  width: 100%;
+`
+
+export const Tag = styled.div`
+  background-color: #ccc;
+  border-radius: 5px;
+  font-size: 0.9rem;
+  padding: 0.5rem;
+  margin: 0.25rem;
+`
+
+export const Footer = styled.footer`
+  display: flex;
+  align-items: center;
+  flex-wrap: wrap;
+`
+
+export const PostDate = styled.div`
+
+`
+
+export const NoteCount = styled.div`
+
+`
